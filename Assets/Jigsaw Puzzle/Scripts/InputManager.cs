08b623b41349d61a132c5f1296b14caac7027719
furkancanc <fcc.ceylan@gmail.com>
2025-03-11 23:19:05 +0300

    Input Manager

---
+++ Assets/Jigsaw Puzzle/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Update()
+    {
+        ManageInput();
+    }
+
+    private void ManageInput()
+    {
+        if (Input.touchCount == 1)
+            ManageSingleInput();
+    }
+    
+    private void ManageSingleInput()
+    {
+        Vector2 touchPosition = Input.touches[0].position;
+        Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+        TouchPhase touchPhase = Input.touches[0].phase;
+
+        switch (touchPhase)
+        {
+            case TouchPhase.Began:
+                // If detect a piece, start moving
+                // If not move camera
+                break;
+            default:
+                break;
+        }
+    }
+}
